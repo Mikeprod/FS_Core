4bcd649a5920980c15ffe68eee543859e097c020
Bastien Clément <g@ledric.me>
2016-04-22 21:43:06 +0200

    Geofence: Module template

---
+++ Geofence.lua
@@ -0,0 +1,16 @@
+local _, FS = ...
+local Geofence = FS:RegisterModule("Geofence")
+
+-------------------------------------------------------------------------------
+-- Life-cycle events
+-------------------------------------------------------------------------------
+
+function Geofence:OnInitialize()
+	
+end
+
+function Geofence:OnEnable()
+end
+
+function Geofence:OnDisable()
+end
